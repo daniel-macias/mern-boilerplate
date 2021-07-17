110f7f28803b8badbd962df4ecbc039e3a1aa176
Daniel Macias <josedanielmr@gmail.com>
2021-07-16 23:52:22 -0600

    added login functionality, doesnt work, push never completes

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,17 +13,47 @@ exports.register = async (req, res, next) => {
         res.status(201).json({
           success: true,
           user: user,
-        })
+        });
       } catch (error) {
         res.status(500).json({
           success: false,
           error: error.message,
-        })
+        });
       }
 }
 
-exports.login = (req, res, next) => {
-    res.send("Login Route")
+exports.login = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if(!email || !password){
+      res.status(400).json({ success: false, error: "Email or password not provided" });
+
+      try{
+        const user = await User.findOne({ email }).select("+password");
+        if(!user){
+          res.status(404).json({ success: false, error: "User not found or invalid" });
+        }
+
+        const isMatch = await user.matchPasswords(password);
+
+        if(!isMatch){
+          res.status(404).json({ success: false, error: "Password not valid" });
+        }
+
+        res.status(200).json({
+          success: true,
+          token: "sampletoken",
+        })
+
+
+
+      }catch (error) {
+        res.status(500).json({
+          success: false,
+          error: error.message,
+        });
+      }
+    }
 }
 
 exports.forgotpassword = (req, res, next) => {
